79b06b3eb9f7d5e47e84a5bebda852f31dbcf6e7
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-01-10 12:06:29 +0530

    Added Variables file

---
+++ variables.js
@@ -0,0 +1,28 @@
+// Here in JS, we will be using "let, var and const"
+
+// "var" is the older way of using variables but with the latest version of JS we will be using "let and const" still we can use "var"
+
+let num1 = 10
+console.log(num1);
+
+num1 = 20
+console.log(num1);
+
+const x1 = 100
+console.log(x1);
+
+// x1 = 200 This cannot be done as we cannot change the value of a constant
+// console.log(x1); 
+
+// It is completely optional to use a semi-colon at the end and even if we don't write it will work fine
+
+/* Multiple lines of 
+ comment can be added within this block 
+  with one star and forward slash */
+
+// Rules for creating variables:
+// 1. Don't use the reserved keywords for as a variable name
+// 2. Don't start the variable name with a number
+// 3. Variable names are case sensitive so, "John", "johN" and "john" will be 2 different variables
+// 4. Avoid using hyphen in variable name
+// 5. Can use underscore
