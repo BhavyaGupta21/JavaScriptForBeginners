10f0d46cd2e4a2c93573f189201de1594a4d79e1
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-01-25 13:21:19 +0530

    Changes the folder structure of the entire project

---
+++ Conditional Statements/ifElseCondition2.js
@@ -0,0 +1,21 @@
+let browser = "Safari"
+if (browser.includes("Chrome")) {
+    console.log("Chrome Started");
+}
+else if (browser.includes("Firefox")) {
+    console.log("Firefox Started");
+}
+else if (browser.includes("Safari")) {
+    console.log("Safari Started");
+}
+else {
+    console.log("Sorry");
+}
+
+let num = 10;
+if (num < 50) {
+    console.log("Fail");
+}
+else {
+    console.log("Pass");
+}
