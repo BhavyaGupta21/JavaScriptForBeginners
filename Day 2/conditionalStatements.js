c8da9ab75194a5dac3d4d599eb7985aec59bb66c
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-01-25 13:07:17 +0530

    Added a file for conditional Statements

---
+++ Day 2/conditionalStatements.js
@@ -0,0 +1,15 @@
+// syntax for if-else condition
+if (false) {
+    console.log("Hello");
+}
+else {
+    console.log("Bye");
+}
+
+let browser = "Chrome"
+if (browser.includes("Chrome")) {
+    console.log("Chrome Started");
+}
+else {
+    console.log("Firefox Started");
+}
