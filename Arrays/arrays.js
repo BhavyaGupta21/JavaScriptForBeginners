d2337a420f8b6bd8b10962c67c08b404a53d3975
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-02-03 12:03:08 +0530

    Added arrays files

---
+++ Arrays/arrays.js
@@ -0,0 +1,110 @@
+// It is a collection of values [10, 20, 30, 40]
+// Index of array always starts from 0
+// Every time we get more than one web element, we will be getting it in an array and so the array that we will be getting will bw the array of web elements. Each element of the array we represent dedicated web element
+
+let arr = [10, 20, 30, 40]
+console.log("Value at array position 2 is: " + arr[2]); // To fetch a specific value from the array
+console.log(arr[100]); // It will through undefined as this index does not exist
+console.log();
+
+// In JS we can have multiple dataTypes inside one array. We can even have another array inside an array
+let arr1 = [10, "John", false, 2.5]
+console.log("value at position 1 of arr1 is: " + arr1[1]);
+console.log();
+
+// Array inside array
+let arr2 = [
+    10,
+    "JS",
+    "John",
+    ["Java", "JS", "C#"] // This will be at index 3
+]
+
+console.log("Value of the array inside another array is: " + arr2[3]);
+console.log("To get value at any index of array inside another array: " + arr2[3][1]);
+console.log();
+
+// Objects inside array
+let arr3 = [
+    10,
+    "JS",
+    person = {
+        fname: "John",
+        lname: "Doe",
+        profession: "QA",
+        phone: 909090,
+        status: true
+    }
+]
+console.log("Value of object inside array: " + arr3[2]);
+console.log(arr3[2].fname); // To capture any value using key of the object inside arrays
+console.log();
+
+// Properties of array
+let arr4 = [10, 20, 30, 40, 50];
+console.log("Length of the array 4 is: " + arr4.length);
+console.log();
+
+// Adding values to the array
+arr4.push(200); // Push will add the given value to the last and array will have the new value as 200 at the end
+console.log(arr4.push(700)); // When we write the push statement inside the log statement, the element gets added and we are returned with the new length of the array
+arr4.unshift(300); // Unshift also adds the value to the array but this will add the given value to the first position of the array
+console.log(arr4);
+console.log();
+
+// Removing values from array
+arr4.pop() // It removes the value from the last of any array
+arr4.shift(); // This will remove a value from the beginning of the array or the first element
+console.log(arr4);
+console.log();
+
+// If we want to add or remove any value from somewhere in the middle of the array, we have a method called splice
+// To add a value using splice
+arr4.splice(1, 2) // Both these values are of index. So, it will remove 2 values from index 1 till 2
+arr4.splice(1, 1); // This will remove the value at index 1 only
+console.log(arr4);
+console.log();
+
+let arr6 = [100, 200, 300, 400, 500]
+arr6.splice(1); // If we give any one value inside the splice method, the array will have only 1 value left that is the first one.
+console.log(arr6);
+arr6.splice(0); // If we give 0 here inside splice, we will be left with no value inside the array
+console.log(arr6);
+console.log();
+
+// To add value using splice
+arr4.splice(2, 0, 500) // At index 2 "500" will be added. We will make second value as "0" purposely because we are using same splice method for adding as well as removing elements from array
+console.log(arr4);
+arr4.splice(3, 0, 'John');
+console.log(arr4);
+console.log();
+
+// Array is also a built-in object in JS with 'n' number of methods inside it
+
+let arr5 = [] // Blank array
+console.log(arr5);
+console.log(arr5.length);
+console.log();
+
+// To update any value in the array
+arr4[3] = 900;
+console.log(arr4);
+
+let arr7 = [10, "John", true, 89.09]
+
+let o1 = {
+    fname: "JS",
+    lanme: "Cypress"
+}
+arr7.push(o1);
+console.log(arr7);
+console.log();
+
+let o2 = {
+    fname: "PW",
+    lname: "WDIO"
+}
+
+arr7.push(o2);
+console.log(arr7);
+console.log();
---
+++ Arrays/iteratingValuesInArray.js
@@ -0,0 +1,13 @@
+let arr = [10, 20, 30, 40]
+for (let i = 0; i < arr.length; i++) {
+    console.log("Value of i is: " + arr[i]);
+}
+
+
+let a1 = [1, 2, 3, 4]
+let a2 = a1;
+console.log(a1);
+console.log(a2);
+a1.push(10);
+console.log(a1);
+console.log(a2); // a2 will also get updated here
