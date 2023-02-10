ed4c5f7970a6a7d61269c0f6dd462f5ae88e14d4
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-02-10 13:01:41 +0530

    Added import and export of a class files

---
+++ ImportAndExport/importClass.js
@@ -0,0 +1,9 @@
+import { Employee } from "./exportClass.js";
+
+let emp1 = new Employee("John", "QA", "USA");
+emp1.empDetails;
+console.log();
+
+let emp2 = new Employee("Doe", "Developer", "Pune");
+emp2.empEmail = "test.test@test";
+emp2.empDetails;
