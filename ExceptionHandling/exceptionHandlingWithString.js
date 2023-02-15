e17b1b3bd51b6eb8e0b7b94fcdf766d69854e2fb
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-02-15 15:16:30 +0530

    Added another file for exception handling

---
+++ ExceptionHandling/exceptionHandlingWithString.js
@@ -0,0 +1,9 @@
+try {
+    let str = "JavaScript is fun to use"
+
+    if(str.startsWith("JavaScript")) {
+        throw "Its not very confusing"
+    }
+} catch (error) {
+    console.log("JavaScript is a good language.");
+}
