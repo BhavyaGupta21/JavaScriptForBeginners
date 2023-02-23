038a7764b811dd678836400e511929d43c02ccaf
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-02-23 16:13:30 +0530

    Added a file for ternanry operator

---
+++ Operators/ternaryOperator.js
@@ -0,0 +1,3 @@
+let authenticated = false;
+
+authenticated ? console.log("SIGNOUT") : console.log("SIGNIN"); // If authenticated has value "true", statement before the colon and after the question mark will run. If the value inside "authenticated" is false, last statement after the colon will run.
