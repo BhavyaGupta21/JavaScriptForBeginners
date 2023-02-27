eba5ae0aa0d5d629a744b6e62b7e028c2189124a
BhavyaGupta21 <guptabhavya2101@gmail.com>
2023-02-27 12:57:31 +0530

    Modified continue file

--- BreakAndContinue/continue.js
+++ BreakAndContinue/continue.js
@@ -4,3 +4,11 @@ for (let i = 0; i <= 10; i++) {
     }
     console.log("Value of i is: " + i);
 }
+
+
+let myStates = ["Himachal Pradesh", "Punjab", "Gujrat", 1947, "Haryana", "Rajasthan"];
+
+for(let i = 0; i < myStates.length; i++) {
+    if(typeof myStates[i] !== 'string') continue; // This will skip any other dataType other than string and print only the string
+    console.log(myStates[i]);
+}
